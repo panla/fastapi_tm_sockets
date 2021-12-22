ac0b258e1c1d5e8749b1af1f27034e2f2c3f2e6a
panla <pankla@163.com>
2021-12-22 10:11:55 +0800

    update sub app name

--- apps/api_messages/__init__.py
+++ apps/api_messages/__init__.py
@@ -15,10 +15,10 @@ def register_routers(app: FastAPI):
 def init_sub_app(app: FastAPI):
     """mount sub app"""
 
-    api_admin_app: FastAPI = FastAPI(include_in_schema=ServiceConfig.INCLUDE_IN_SCHEMA)
+    api_app: FastAPI = FastAPI(include_in_schema=ServiceConfig.INCLUDE_IN_SCHEMA)
 
-    register_exception(api_admin_app)
-    register_routers(api_admin_app)
-    app.mount(path='/api-sockets', app=api_admin_app, name='api_sockets_app')
+    register_exception(api_app)
+    register_routers(api_app)
+    app.mount(path='/api-sockets', app=api_app, name='api_messages_app')
 
     return app
